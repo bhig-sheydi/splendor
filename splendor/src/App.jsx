89416cd371d3b615a1a0a5116c35534fe7c459be
bhig-sheydi <bhigsheydi@gmail.com>
2025-04-23 09:58:09 -0700

    its your tuen

--- splendor/src/App.jsx
+++ splendor/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./page/Home.JSX";
+import Home from "./Page/Home";
 import Navbar from "./component/Navbar";
 
 
